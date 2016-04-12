3f16cf9ed6275c436336f818354699dd98160841
hy <hy@Lenovo-hy>
2016-04-13 00:14:39 +0800

    [hy]-增加广告实体类1.0

---
+++ src/com/edu/ads/bean/ggp/Ggplx.java
@@ -0,0 +1,63 @@
+package com.edu.ads.bean.ggp;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Table(name="t_ggplx")
+@Entity
+public class Ggplx {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "c_id", nullable = false, length = 32)
+	private String id;
+	
+	@Column(name = "C_MC", nullable = true, length = 300)
+	private String mc;
+	
+	@Column(name = "C_MS", nullable = false, length = 900)
+	private String ms;
+	
+	@OneToMany(cascade=CascadeType.ALL,mappedBy="lx")
+	private Set<Ggp> ggps;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Set<Ggp> getGgps() {
+		return ggps;
+	}
+
+	public void setGgps(Set<Ggp> ggps) {
+		this.ggps = ggps;
+	}
+
+	public String getMs() {
+		return ms;
+	}
+
+	public void setMs(String ms) {
+		this.ms = ms;
+	}
+
+	public String getMc() {
+		return mc;
+	}
+
+	public void setMc(String mc) {
+		this.mc = mc;
+	}
+
+}
