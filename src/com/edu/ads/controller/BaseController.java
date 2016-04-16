7fc139e5e304f94a8c9433e4789d4a8787382da9
wjc <wjc@lonelyboy>
2016-04-16 15:07:37 +0800

    [wjc]-增加bulidPage方法

--- src/com/edu/ads/controller/BaseController.java
+++ src/com/edu/ads/controller/BaseController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.ServletRequestDataBinder;
 
 import com.edu.ads.bean.user.User;
+import com.edu.ads.common.page.Page;
 
 public class BaseController {
 
@@ -21,7 +22,32 @@ public class BaseController {
 		binder.bind(request);
 	}
 	
+	/**
+	 * 获取登录用户
+	 * @param request
+	 * @return
+	 */
 	public User getLoginUser(HttpServletRequest request){
 		return (User)request.getSession().getAttribute("user");
+	}
+	
+
+	/**
+	 * 创建page
+	 * @param currentPage
+	 * @param pageSize
+	 * @return
+	 */
+	public Page bulidPage(String currentPage,String pageSize){
+		int current = 0;
+		int size = 5;
+		try{
+			 current = Integer.valueOf(currentPage);
+			 size = Integer.valueOf(pageSize);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return  new Page(current,size);
 	}
 }
